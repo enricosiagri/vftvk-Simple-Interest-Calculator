531db3b865f8d312a978de00a6a7d94960ba9f59
EnricoDataSci <enrico.siagri@gmail.com>
2021-03-25 17:30:48 +0100

    middleground

--- script.js
+++ script.js
@@ -5,11 +5,23 @@ function compute()
   var year = Number(document.getElementById("Years").value);
   var rate = Number(document.getElementById("Interest").value);
 
+  var inter = 0;
 
   for (i=0; i<year; i++) {
-    inter = (amount*rate*0.01);
     amount = amount+inter;
+    inter = (amount*rate*0.01);
   }
 
-  document.getElementById("result").innerHTML = amount;
+  document.getElementById("result2").innerHTML = inter.toFixed(2);
+}
+
+function compute_easy() {
+
+  var amount = Number(document.getElementById("amount").value);
+  var year = Number(document.getElementById("Years").value);
+  var rate = Number(document.getElementById("Interest").value);
+
+  inter = (amount*rate*0.01*year);
+
+  document.getElementById("result1").innerHTML = inter.toFixed(2);
 }
